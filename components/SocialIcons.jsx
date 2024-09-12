000d8bcf4848f3a343aa38a14d9120361438cc4a
jai <jaijai.stell@gmail.com>
2024-09-12 01:04:19 -0700

    ugh

--- components/SocialIcons.jsx
+++ components/SocialIcons.jsx
@@ -13,7 +13,7 @@ const SocialIcons = () => {
       <a href="https://docs.google.com/document/d/1VLkC0Zy2qz4mmUR__-OPIQ1Gn7AhhFv5b41P0xn1PIc/edit?usp=sharing" className="text-white hover:text-red-500 transition-colors duration-300" aria-label="Google Docs" title="Google Docs">
         <FaGoogleDrive className="w-10 h-10" />
       </a>
-      <a href="https://dev.to/jstellmacher" className="text-white hover:text-green-500 transition-colors duration-300" aria-label="Dev.to" title="Dev.to">
+      <a href="https://dev.to/jstellmacher" className="text-white hover:text-gray-800 transition-colors duration-300" aria-label="Dev.to" title="Dev.to">
         <FaDev className="w-10 h-10" />
       </a>
     </div>
