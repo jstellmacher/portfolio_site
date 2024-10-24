11999045b8259befb7294e6f16e113208a763e07
jai <jaijai.stell@gmail.com>
2024-10-24 16:21:36 -0700

    footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,12 +5,15 @@ const Footer = ({ name = 'Jai Stellmacher', links = [] }) => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="dark:text-white py-6 relative">
-      <div className="container mx-auto flex justify-center items-center">
-        <p className="text-sm">© {currentYear} {name}. All rights reserved.</p>
-      </div>
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-6">
-        <Link href="/credits" className="inline-block px-4 py-2 bg-white/10 text-white rounded-lg backdrop-blur-md hover:bg-white/20 transition">
+    <footer className="py-6 relative">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 space-x-6"> {/* Added space-x-6 */}
+        <p className="text-sm text-gray-700 dark:text-gray-100">
+          © {currentYear} {name}. All rights reserved.
+        </p>
+        <Link
+          href="/credits"
+          className="inline-block px-4 py-2 bg-white/20 text-gray-700 dark:text-gray-100 border border-gray-400 dark:border-white rounded-lg shadow-md dark:shadow-lg backdrop-blur-md hover:bg-white/40 transition transform hover:scale-105 active:scale-95"
+        >
           View Credits
         </Link>
       </div>
